e700de3378bc03e7adb882ef4dbead8b82d75b65
st120712 <zzw123@naver.com>
2025-05-06 21:16:50 +0900

    log 저장 위치 변경

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from scheduler.base_scheduler import run_scheduler
 import logging
+import os
+
+log_path = "/logs/threshold-calculator.log"
+os.makedirs(os.path.dirname(log_path), exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,
     format='[%(asctime)s] %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("threshold.log"),
+        logging.FileHandler(log_path),
         logging.StreamHandler()
     ]
 )
