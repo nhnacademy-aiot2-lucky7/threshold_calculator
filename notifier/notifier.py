d427ae849459abc36a3a543c75c9e01ad3a2d31c
st120712 <zzw123@naver.com>
2025-06-09 22:24:38 +0900

    .

--- notifier/notifier.py
+++ notifier/notifier.py
@@ -5,11 +5,8 @@ from config import config
 gateway_url = config.GATEWAY_SERVICE_URL
 
 def notify_rule_engine(gateway_id):
-    payload = {
-        "gatewayId": gateway_id
-    }
     try:
-        response = requests.put(f"{gateway_url}/gateways/threshold-status", json=payload)
+        response = requests.put(f"{gateway_url}/gateways/threshold-status", json=gateway_id)
         response.raise_for_status()
         logging.info(f"[WEBHOOK] gateway-service update threshold status 통보 완료: {response.status_code}")
     except requests.exceptions.RequestException as e:
